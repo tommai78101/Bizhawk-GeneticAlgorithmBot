3fb99d0d6e3f61fa0c65189296139b5fa44fe6a3
tom_mai78101 <tom.mai78101@gmail.com>
2022-07-28 21:14:13 -0400

    No need to keep this code logic anymore, now that the issue is resolved.

--- run_build.cmd
+++ run_build.cmd
@@ -1,7 +1,2 @@
 cd BizHawk
-if not exist config_bak.ini (
-	cp config.ini config_bak.ini
-) else (
-	cp config_bak.ini config.ini
-)
 .\EmuHawk.exe --open-ext-tool-dll=GeneticAlgorithmBot
--- run_config_restore.cmd
+++
@@ -1,6 +0,0 @@
-cd BizHawk
-if not exist config_bak.ini (
-	cp config.ini config_bak.ini
-) else (
-	cp config_bak.ini config.ini
-)
