1e9694f4949dd0ece05a0d8aaaa72a59dfdcd91e
tom_mai78101 <tom.mai78101@gmail.com>
2023-02-13 23:13:25 -0500

    Tidying up some build issues. Now the GA bot can be built just by running the run_build.cmd batch file.

--- run_build.cmd
+++ run_build.cmd
@@ -1,4 +1,29 @@
+@echo off
+set PATH=%PATH%;C:\Program Files (x86)\git\bin;C:\Program Files\git\bin
+git --version > NUL
+@if errorlevel 1 goto MISSINGGIT
+
+dotnet build .\src\GeneticAlgorithmBot.sln -c Release --no-incremental > NUL
+@if not errorlevel 0 goto DOTNETBUILDFAILED
+
+dotnet build .\src\GeneticAlgorithmBot.sln -c Release
+@if errorlevel 1 goto DOTNETBUILDFAILED
+
 cp .\src\bin\Release\net48\GeneticAlgorithmBot.dll .\BizHawk\ExternalTools\
 
 cd BizHawk
 .\EmuHawk.exe --open-ext-tool-dll=GeneticAlgorithmBot
+goto END
+
+:DOTNETBUILDFAILED
+set ERRORLEVEL=1
+@echo dotnet build failed. Usual cause: user committed broken code, or unavailable dotnet sdk
+goto END
+
+:MISSINGGIT
+set ERRORLEVEL=1
+@echo missing git.exe. can't make distro without that.
+
+:END
+
+
